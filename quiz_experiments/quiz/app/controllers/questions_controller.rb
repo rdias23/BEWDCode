7f65bcf82efee08600591f81cb102f06e5ff375c
Raymond Dias <rdias23@gmail.com>
2013-06-04 17:37:10 -0400

    building test quiz and adding a couple links for the rewsly homework

---
+++ quiz_experiments/quiz/app/controllers/questions_controller.rb
@@ -0,0 +1,24 @@
+class QuestionsController < ApplicationController
+def index
+  @questions = Question.all
+  @questions.sort! {|a,b| a.text <=> b.text}
+end
+def new
+  @question = Question.new
+end
+def create
+  questions_params = params.require('question').permit(:text)  
+  @question = Question.new(questions_params)
+
+  if @question.save
+    redirect_to(@question, :notice => 'Question was successfully created.')
+  else
+    render :action => "new"
+  end
+end
+def show
+  @question = Question.find(params[:id])
+
+end  
+
+end
