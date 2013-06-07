dccc60d031c3952391d4ba79a547efd95f45c9ef
Raymond Dias <rdias23@gmail.com>
2013-06-07 18:07:35 -0400

    update to quiz project experiment

--- quiz_experiments/quiz/config/initializers/session_store.rb
+++ quiz_experiments/quiz/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Quiz::Application.config.session_store :cookie_store, key: '_quiz_session'
+Quiz::Application.config.session_store :active_record_store
