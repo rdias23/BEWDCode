394cc4a74d11d75e279644b270ed4b62dcf082aa
Raymond Dias <rdias23@gmail.com>
2013-06-09 20:51:46 -0400

    did some more devise stuff

--- quiz_experiments/quiz/db/schema.rb
+++ quiz_experiments/quiz/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130609022736) do
+ActiveRecord::Schema.define(version: 20130609231615) do
 
   create_table "choices", force: true do |t|
     t.string   "text"
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(version: 20130609022736) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "house_allegiance"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
